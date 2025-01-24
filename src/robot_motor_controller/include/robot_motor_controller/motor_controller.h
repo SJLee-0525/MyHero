1f3477da5b421a91122314bdbb4c383fb51ec3f9
노규헌 <mark_er@naver.com>
2025-01-24 14:28:37 +0900

    :sparkles:[Feat] 모터 현재 상태 토픽 퍼블리쉬

--- src/robot_motor_controller/include/robot_motor_controller/motor_controller.h
+++ src/robot_motor_controller/include/robot_motor_controller/motor_controller.h
@@ -30,16 +30,32 @@ private:
 
     // ROS 파라미터
     int servo_channel_;
-    sensor_msgs::JointState joint_state_msg_;
 
-    // 차량 축간거리
-    static constexpr float WHEELBASE = 0.20; // 20cm = 0.20m로 설정
+    // ================ RViz URDF용 ===========================
+    // 바퀴 회전각 누적값(시각화용)
+    float fl_wheel_angle_; // front_left
+    float fr_wheel_angle_; // front_right
+    float rl_wheel_angle_; // rear_left
+    float rr_wheel_angle_; // rear_right
+
+    // 가장 최근에 업데이트된 시간 (delta time 계산용)
+    ros::Time last_update_time_;
+
+    // 바퀴 반지름 (m 단위) - URDF상 0.035
+    float wheel_radius_;
+
+    // throttle=1.0일 때의 최대 선속도 (m/s)
+    float max_speed_mps_;
+
+    // JointState 메시지
+    sensor_msgs::JointState joint_state_msg_;
+    // =========================================================
 
 public:
     MotorController(ros::NodeHandle &nh);
     ~MotorController(); // 소멸자 추가
     void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg);
-    void publishJointStates(float steering_angle, float throttle);
+    void publishJointStates(float steering_angle_deg, float throttle, double dt);
 };
 
 #endif // MOTOR_CONTROLLER_H

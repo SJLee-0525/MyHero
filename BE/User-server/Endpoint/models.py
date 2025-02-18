3ada0c2448799bd826d3c636a79520070ad629c3
itdice <itdiceofficial@gmail.com>
2025-02-18 12:59:27 +0900

    :sparkles: [Feat] 로컬 저장소에 담긴 Session ID를 보내면 브라우저에 담기게 해주는 기능 추가

--- BE/User-server/Endpoint/models.py
+++ BE/User-server/Endpoint/models.py
@@ -60,6 +60,12 @@ class IDCheck(BaseModel):
     """
     id: Optional[str] = None  # 확인 및 점검을 위한 ID
 
+class SessionCheck(BaseModel):
+    """
+    Session이 유효한지 확인하기 위해 Client가 보내는 데이터
+    """
+    session_id: Optional[str] = None  # 재 로그인을 위한 Session ID
+
 class Family(BaseModel):
     """
     가족 생성을 위해 client가 보내는 데이터
